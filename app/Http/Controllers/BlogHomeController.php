411b4df2a322b56a9b9664d1274e8ca3ab237205
Ernesto Villafuerte <ernesto.villafuerte@gmail.com>
2016-11-21 09:56:30 -0200

    primeiro commit de laravel-express
    primeiro commit de laravel-express

---
+++ app/Http/Controllers/BlogHomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TestController extends Controller
+{
+    //
+    function index(){
+        $posts=(0->('','',''));
+    }
+}
